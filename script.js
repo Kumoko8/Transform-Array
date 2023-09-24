20a955448048dc327c4c8d6b81222a28ec5d2f04
Kumori <tauen8@gmail.com>
2023-09-24 16:41:12 -0500

    editing array defintion and input and output arrays

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
 var map = function(arr, fn) {
     for (let i = 0; i < arr.length; i++) {
         returnArray = fn(arr[i], i)        
     }
+   
     
     return returnArray
 };
+function plusOne(n) {
+    return n +1;
+}
+const inputArr = [1,2,3]
+
+const result = map(inputArr, plusOne)
+console.log(result);
